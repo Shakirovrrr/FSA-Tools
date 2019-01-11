b5ace90dae368c478bb817c96329836f0568e76f
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-01-11 20:34:23 +0300

    Update Main and add FSA describing lang

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		FileInputStream inputStream = new FileInputStream("fsa.txt");
+		FileInputStream inputStream = new FileInputStream("onoff.fsa");
 		FSAStreamParser parser = new FSAStreamParser(inputStream);
 		inputStream.close();
 		FSABuilder builder;
@@ -21,7 +21,11 @@ public class Main {
 				FSABuildResult buildResult = builder.build();
 				if (buildResult.successful()) {
 					automata = buildResult.getAutomata();
+				} else {
+					buildResult.getErrors().forEach(System.out::println);
 				}
+			} else {
+				System.err.println("Can't parse");
 			}
 		}
 
